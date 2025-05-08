b4ca797cd31c6f28e15d130b5c6832a140b004c8
KIKW12 <a01711235@tec.mx>
2025-05-08 15:43:11 -0600

    updated color

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,12 +9,12 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
           rel="stylesheet"
         />
-        <link rel="icon" href="/EA.ico" />
+        <link rel="icon" href="/EA.png" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <meta name="description" content="Enrique Ayala - Computer Science Student & Back-End Developer" />
-        <meta name="theme-color" content="#0ea5e9" />
+        <meta name="theme-color" content="#fee715" />
       </Head>
       <body>
         <Main />
